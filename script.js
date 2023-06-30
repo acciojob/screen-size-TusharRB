de8244bbccf25605c3ba927b2896a2ad80fca2b4
TusharRB <91899841+TusharRB@users.noreply.github.com>
2023-06-30 13:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateSizeInfo() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const sizeInfo = document.getElementById('sizeInfo');
+  
+  sizeInfo.textContent = `Width: ${width}px, Height: ${height}px`;
+}
+
+window.addEventListener('resize', updateSizeInfo);
